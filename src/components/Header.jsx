3462eff9c4b72d8e67bc8dc4713b692f3ef86ecb
laodeshaldanfalih <77shaldan77@gmail.com>
2023-08-07 22:57:18 +0700

    feat

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Creative from "../assets/Creative.png"
 const Header = () => {
     return (
         <div className="w-full mx-auto text-center flex flex-col">
-            <div className="gap-[24px] max-w-[1000px] flex flex-col py-[80px] mx-auto">
+            <div className="gap-[24px] max-w-[1000px] flex flex-col py-[80px] mx-auto  px-4">
                 <h1 className="font-playfair font-normal text-display-lg md:text-display-2xl">Make <span className="italic">your home</span> an ode to joy</h1>
                 <p className="font-Outfit text-body-lg font-light">Kami mengubah rumah kosong menjadi hunian cantik, memaksimalkan area-area kecil dengan furnitur yang cocok. Kami mewujudkan cita rasa unik Anda menjadi kenyataan!</p>
             </div>
